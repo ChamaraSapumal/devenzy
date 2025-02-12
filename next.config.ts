96d7565191abd710a44ef384fdf4c0e5c84f055f
ChamaraSapumal <chamarasecu.21@gmail.com>
2025-02-12 21:21:49 +0530

    Change Next.js output configuration from 'standalone' to 'export'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  output: 'standalone',
+  output: 'export',
   basePath: "/devenzy",
   assetPrefix: "/devenzy/",
   trailingSlash: true,
